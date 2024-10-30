f166326a05635adfb0578b63d38c7096a1f13cdb
Arbaz Khan <turkishpasha007@gmail.com>
2024-10-30 13:34:57 +0530

    Refactoring- fetch games with React Query

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -3,8 +3,8 @@ import { SiNintendo } from 'react-icons/si'
 import { MdPhoneIphone } from 'react-icons/md'
 import { BsGlobe } from 'react-icons/bs'
 import { IconType } from "react-icons";
-import { Platform } from "../hooks/useGames";
 import { HStack, Icon } from "@chakra-ui/react";
+import { Platform } from '../hooks/usePlatforms';
 
 interface Props {
   platforms: Platform[];
