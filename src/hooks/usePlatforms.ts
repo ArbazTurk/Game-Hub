0ded41ae8587877eb691c40a56a9ea5f81677b8a
Arbaz Khan <turkishpasha007@gmail.com>
2024-10-31 12:54:15 +0530

    Refactor- create lookup hooks

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,6 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import platforms from "../data/platforms";
-import APIClient, { FetchResponse } from "../services/api-client";
+import APIClient from "../services/api-client";
 // import useData from "./useData";
 
 const apiClient = new APIClient<Platform>("/platforms/lists/parents");
@@ -23,7 +23,8 @@ const usePlatforms = () =>
     //     .then((res) => res.data),
     queryFn: apiClient.getAll,
     staleTime: 24 * 60 * 60 * 1000, //24h
-    initialData: { count: platforms.length, results: platforms },
+    // initialData: { count: platforms.length, results: platforms, next: null },
+    initialData: platforms,
   });
 
 export default usePlatforms;
