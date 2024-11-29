2c0f82b6831fe167b2f79c185c337aed8cca5dfa
Arbaz Khan <turkishpasha007@gmail.com>
2024-11-29 19:21:50 +0530

    Refactor entity interfaces

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -11,7 +11,7 @@ import { MdPhoneIphone } from "react-icons/md";
 import { BsGlobe } from "react-icons/bs";
 import { IconType } from "react-icons";
 import { HStack, Icon } from "@chakra-ui/react";
-import { Platform } from "../entities/Platform";
+import Platform from "../entities/Platform";
 
 interface Props {
   platforms: Platform[];
